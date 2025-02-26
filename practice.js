9ef4732d2b1f60784c90dd7ede58e7890c9b1556
Rama subramanian <86415863+codewithrama@users.noreply.github.com>
2025-02-26 05:30:04 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,32 @@
+// // var input=prompt("Enter Your Name");
+// // alert("Hello "+input);
+
+// // var your_msg=prompt("Enter your message");
+
+// // alert("Your message is "+your_msg.length+" characters long");
+
+// // alert("Your message lastletter: "+ your_msg.slice(-1));
+
+
+// // var input=prompt("Enter Your Name");
+// // alert("Hello " + input.slice(0, 1).toUpperCase() + input.slice(1));
+
+// let sentence = "JavaScript is amazing!";
+
+
+var age = prompt("Enter your age");
+
+var life = 90;
+var days = life * 365;
+var weeks = life * 52;
+var months = life * 12;
+
+var remainingDays = days - (age * 365);
+var remainingWeeks = weeks - (age * 52);
+var remainingMonths = months - (age * 12);
+
+console.log("You have " + remainingDays + " days, " + remainingWeeks + " weeks, and " + remainingMonths + " months left.");
+
+
+
+
